d2357af1abff75cda2f07576a1bbd5ad3b167567
jolumley <49235579+jolumley@users.noreply.github.com>
2020-01-29 13:15:42 +0000

    epact2 routing

--- app/routes.js
+++ app/routes.js
@@ -5,3 +5,21 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+router.get(/orgType/, function (req, res) {
+    if (req.query.incomesingle === "gp-practice") {
+      res.redirect('epact-gp-details-auth'); 
+    } else if (req.query.incomesingle === "primary-care-network") {
+      res.redirect('epact-pcn-access');
+    } else if (req.query.incomesingle === "trust-other") {
+      res.redirect('epact-trust-access');
+    } else if (req.query.incomesingle === "commissioning-support-unit") {
+      res.redirect('epact-which-csu');
+    } else if (req.query.incomesingle === "national-orgs") {
+      res.redirect('epact-national-access');
+    } else if (req.query.incomesingle === "other-orgs") {
+      res.redirect('epact-which-org');
+    } else {
+      return;
+    }
+  });
